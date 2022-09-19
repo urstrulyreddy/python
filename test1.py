99a9e05e69e03224fdb177cddf5472573dadd034
urstrulyreddy <96733257+urstrulyreddy@users.noreply.github.com>
2022-09-19 22:10:28 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,12 @@
+num = 7
+amt=123.45
+code='A'
+pi = 3.1415926536
+population_of_India=1000000000
+msg = "hi"
+
+print("num="+str(num))
+print("\n amt="+str(amt))
+print("\n code="+str(code))
+print("\n population="+str(population_of_India))
+print("\n message="+str(msg))
