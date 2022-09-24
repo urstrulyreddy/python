570775b949f4aabd3e74e09a8484cfb39392ca67
urstrulyreddy <96733257+urstrulyreddy@users.noreply.github.com>
2022-09-24 21:27:40 +0530

    Add files via upload

---
+++ test5.py
@@ -0,0 +1,24 @@
+#test3.py
+a=5
+b=2.3
+c=2+3j
+d=b>a
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+b=complex(a,c)
+c=int(a)
+print(a)
+print(b)
+print(c)
+print(d)
+list=[1,2,3,4]
+print(type(list))
+print(list)
+tuple=(5,6,7,8) #tuple doesnt support item assignment
+print(type(tuple))
+print(tuple)
+set={9,0,1,2}
+print(type(set))
+print(set)
